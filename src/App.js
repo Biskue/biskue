a7cf35b2bbd15d1646ab72e0325875b1bd963f6a
--global <--global>
2018-09-29 13:57:38 -0600

    basic structure of landing page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  Landing  from './Views/LandingPage/Landing';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">taylor's branch</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </header> */}
+        <Landing />
       </div>
     );
   }
