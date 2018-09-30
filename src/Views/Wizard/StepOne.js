5d553ef1da62b04e48c412c8dec401fc52148090
Taylor Jackson <tjack94@hotmail.com>
2018-09-30 10:37:13 -0600

    added redux for step one of the wizard

--- src/Views/Wizard/StepOne.js
+++ src/Views/Wizard/StepOne.js
@@ -1,50 +1,120 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getLatitudeLongitude } from './GeoCodeHelper';
+import * as Actions from '../../Redux/Actions/actions';
 
- class StepOne extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      location: '',
-      distance: 10,
-      priceRange: [],
-      latitude: null,
-      longitude: null,
-    }
-  }
-  handleChange(event, name) {
+class StepOne extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			address: '',
+			distance: 10,
+			priceRange: [],
+			latitude: null,
+			longitude: null
+		};
+	}
+	handleChange(event, name) {
 		const value = event.target.value;
 		this.setState({ [name]: value });
-  }
-  
-  getLocation = (e) => {window.navigator.geolocation.getCurrentPosition(function(position) {
-  this.setState({latitude: position.coords.latitude, longitude: position.coords.longitude});
-});}
-handlePriceChange(e){
-  const priceRange = this.state.priceRange
-if(priceRange.includes(e.target.value)){
-  
+	}
+
+	getLocation = () => {
+		console.log('checking location');
+		window.navigator.geolocation.getCurrentPosition((position) => {
+			console.log(position);
+
+			this.setState({ latitude: position.coords.latitude, longitude: position.coords.longitude });
+		});
+	};
+	handlePriceChange(e) {
+		const priceRange = this.state.priceRange;
+		if (priceRange.includes(e.target.value)) {
+			priceRange.splice(priceRange.indexOf(e.target.value), 1);
+		} else {
+			priceRange.push(e.target.value);
+		}
+		this.setState({ priceRange: priceRange });
+	}
+	getLocationByAddress(address) {
+		getLatitudeLongitude(address).then((response) => console.log(response));
+	}
+	render() {
+		return (
+			<div>
+				<input
+					type="text"
+					placeholder="Enter City and State or Zip"
+					onChange={(e) => this.handleChange(e, 'address')}
+				/>
+				<button onClick={() => this.getLocationByAddress(this.state.address)}>Search</button>
+				<div>
+					<button onClick={this.getLocation}>Use Current Location</button>
+				</div>
+				<div>
+					<b>Distance:</b>
+					<input
+						type="range"
+						min={5}
+						max={25}
+						value={this.state.distance}
+						step={5}
+						onChange={(e) => this.handleChange(e, 'distance')}
+					/>{' '}
+					{this.state.distance + ' Miles'}
+				</div>
+				<div>
+					<b>Price Range</b>
+					$10 Or less{' '}
+					<input
+						type="checkbox"
+						value={1}
+						onClick={(e) => {
+							this.handlePriceChange(e);
+						}}
+					/>
+					$11-$30{' '}
+					<input
+						type="checkbox"
+						value={2}
+						onChange={(e) => {
+							this.handlePriceChange(e);
+						}}
+					/>
+					$31-$60{' '}
+					<input
+						type="checkbox"
+						value={3}
+						onChange={(e) => {
+							this.handlePriceChange(e);
+						}}
+					/>
+					$61+{' '}
+					<input
+						type="checkbox"
+						value={4}
+						onChange={(e) => {
+							this.handlePriceChange(e);
+						}}
+					/>
+				</div>
+				<div>
+					<Link to="/wizard/step-2">
+						<button onClick={() => this.saveWhereToState()}>Next</button>
+					</Link>
+				</div>
+			</div>
+		);
+	}
+	saveWhereToState() {
+		const results = {
+			latitude: this.state.latitude,
+			longitude: this.state.longitude,
+			radius: this.state.distance,
+			priceRange: this.state.priceRange
+		};
+		this.props.saveStepOne(results);
+	}
 }
-}
-  render() {
-   
-    return (
-      <div>
-       <input type="text" placeholder= 'Enter City and State or Zip' />
-       <button onClick= {(e)=> this.getLocation}>Use Current Location</button>
-       <div>
-       <b>Distance:</b>
-       <input type="range" min={5} max ={25} value={this.state.distance} step={5} onChange={(e)=> this.handleChange(e, 'distance')} /> {this.state.distance + " Miles"}
-       </div>
-       <div>
-         <b>Price Range</b>
-          $10 Or less <input type="checkbox" value = {1} onChange={this.handleChange}/>
-          $11-$30 <input type="checkbox" value = {2}/>
-          $31-$60 <input type="checkbox" value ={3}/>
-          $61+ <input type="checkbox" value= {4}/>
-       </div>
-      </div>
-    )
-  }
-}
-export default connect(state => state)(StepOne)
+export default connect((state) => state, Actions)(StepOne);
--- src/Views/Wizard/StepTwo.js
+++ src/Views/Wizard/StepTwo.js
@@ -4,7 +4,7 @@ export default class StepTwo extends Component {
   render() {
     return (
       <div>
-        
+        step two
       </div>
     )
   }
