35dd2a14fd7c87db71bb4c0e6b941217fe58ca7f
--global <--global>
2018-10-02 19:33:26 -0600

    updated new register route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import  Landing  from './Views/LandingPage/Landing';
+import 'reset-css';
 
-=======
 import Routes from './Routes'
->>>>>>> master
 
 class App extends Component {
   render() {
     return (
 
       <div className="App">
-<<<<<<< HEAD
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">taylor's branch</h1>
-        </header> */}
-        <Landing />
-=======
+        
       <Routes />
->>>>>>> master
       </div>
     );
   }
--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,13 @@ import Landing from './Views/LandingPage/Landing';
 import LivePoll from './Views/LivePoll/LivePoll';
 import UserInfo from './Views/UserInfo/UserInfo';
 import Wizard from './Views/Wizard/Wizard';
+import Register from './Views/RegisterPage/Register';
 
 export default () => {
 	return (
 		<div>
 			<Route path="poll/:pollCode" component={LivePoll} />
+			<Route path="/register" component={Register} />
 			<Route path="/user-info" component={UserInfo} />
 			<Route path="/wizard" component={Wizard} />
 			<Route exact path ="/" component={Landing} />
