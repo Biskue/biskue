a5b1eb538ae45b65833b69903228c777e2a15415
--global <--global>
2018-10-04 20:27:10 -0600

    created login component and updated routes

--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,14 @@ import LivePoll from './Views/LivePoll/LivePoll';
 import UserInfo from './Views/UserInfo/UserInfo';
 import Wizard from './Views/Wizard/Wizard';
 import Register from './Views/RegisterPage/Register';
+import Login from './Views/LoginPage/Login';
 
 export default () => {
 	return (
 		<div>
 			<Route path="poll/:pollCode" component={LivePoll} />
 			<Route path="/register" component={Register} />
+			<Route path="/login" component={Login} />
 			<Route path="/user-info" component={UserInfo} />
 			<Route path="/wizard" component={Wizard} />
 			<Route exact path ="/" component={Landing} />
