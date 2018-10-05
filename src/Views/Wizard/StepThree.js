617d3b9dac840c1233ec1ed0f56d28f171449e18
Taylor Jackson <tjack94@hotmail.com>
2018-10-04 19:14:32 -0600

    connected step three to redux

--- src/Views/Wizard/StepThree.js
+++ src/Views/Wizard/StepThree.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as Actions from '../../Redux/Actions/actions';
+import axios from 'axios'
 
-export default class StepThree extends Component {
+ class StepThree extends Component {
+   constructor(props){
+    super(props)
+    this.state = {
+
+    }
+   }
+   componentWillMount(){
+     axios.get()
+   }
   render() {
     return (
       <div>
@@ -9,3 +22,4 @@ export default class StepThree extends Component {
     )
   }
 }
+export default connect(state => state, Actions)(StepThree)
