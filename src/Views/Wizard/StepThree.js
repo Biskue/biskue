924e673e9534d501d92eb1c29aedb1ad162962e5
Taylor Jackson <tjack94@hotmail.com>
2018-10-04 20:50:46 -0600

    added step 3 to wizard

--- src/Views/Wizard/StepThree.js
+++ src/Views/Wizard/StepThree.js
@@ -3,21 +3,38 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as Actions from '../../Redux/Actions/actions';
 import axios from 'axios'
+import RestaurantCard from './RestaurantCard'
 
  class StepThree extends Component {
    constructor(props){
     super(props)
     this.state = {
-
+      restaurants: [],
+      selected: [],
     }
    }
    componentWillMount(){
-     axios.get()
+     const {latitude, longitude, radius, categories, priceRange, date} = this.props
+     axios.get(`/poll/search?latitude=${latitude}&longitude=${longitude}&radius=${radius}&open_at=${date}&categories=${categories}&price=${priceRange}`)
+     .then(results => this.setState({restaurants: results.data.businesses}))
+   }
+   addRestaurant = (restaurant)=>{
+    const selected = this.state.selected
+    selected.push(restaurant)
+    this.setState({selected})
    }
   render() {
+    const restaurantsList = this.state.restaurants.map((restaurant, index)=>{
+      return(
+        <RestaurantCard key ={index} addRestaurant={this.addRestaurant} currentRes= {restaurant}/>
+      )
+    })
+    const doneButton = this.state.selected.length > 0 ?( <Link to='/wizard/step-4'><button>Done</button></Link>): null
     return (
       <div>
-        
+        <h2>Select Restaurants to add to Poll</h2>
+        {doneButton}
+        {restaurantsList}
       </div>
     )
   }
