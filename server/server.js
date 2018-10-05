1d366d8756ab7d22e3e1f60e1c3defc2893ce1e4
breakingBrad <bradvanorman@gmail.com>
2018-10-04 21:11:11 -0600

    initial socket.io setup

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 
 require('dotenv').config({
     path: __dirname + '/../.env',
@@ -7,7 +7,9 @@ require('dotenv').config({
 const globalDecorator = require('./middleware/global-decorator.middleware');
 const routerHub = require('./routers/hub.router');
 
-const app = express()
+const app = express();
+const server = require('http').createServer(app);  
+const io = require('socket.io')(server);
 
 globalDecorator(app);
 
@@ -19,6 +21,33 @@ app.use((err, req, res, next) => {
 
 const port = process.env.SERVER_PORT || 4000;
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server listening at localhost:${port}`);
 }); 
+
+app.get('/', function (req, res) {
+    res.send({ response: 'I am alive'}).status(200);
+  });
+  
+  io.on('connection', socket => {
+
+    socket.emit('news', { hello: 'world' });
+
+    socket.on('room', room => {
+        console.log(`Joining Socket Room ${room}`)
+        socket.join(room);
+    })
+
+    socket.on('connect', data => console.log(data));
+
+    socket.on('message', (msg) => { console.log(msg) });
+
+    socket.on('increment', (number, pollCode) => { 
+        console.log(number);
+        const incremented = number + 1;
+        io.sockets.in(pollCode).emit('incremented', incremented);
+    });
+
+    socket.on('disconnect', () => console.log('client disconnected'));
+
+});
--- src/Views/LivePoll/LivePoll.js
+++ src/Views/LivePoll/LivePoll.js
@@ -1,10 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client';
+const socket = socketIOClient('http://localhost:4005/', {
+  extraHeaders: { 'Access-Control-Allow-Credentials': 'omit' }
+});
+
 
 export default class LivePoll extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: '',
+      number: 0,
+      pollCode: this.props.match.params.pollCode,
+    }
+
+  }
+
+  componentDidMount() {
+    var room = this.state.pollCode;
+    socket.emit('room', room);
+    socket.on('news', data  => {
+      this.setState({
+        response: data,
+      })
+      console.log(data);
+      socket.send('hi');
+
+      socket.on('incremented', (number) => {
+      this.setState({
+        number
+      })
+    });
+
+  });
+  }
+
+  increment() {
+    socket.emit('increment', this.state.number, this.state.pollCode)
+  }
+
   render() {
     return (
       <div>
-        
+        Hello 
+        {this.state.response.hello}
+        <button
+          onClick={() =>this.increment() }>Increment Socket</button>
+        {this.state.number}
       </div>
     )
   }
