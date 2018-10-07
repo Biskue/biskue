7c16ebe5c76fc1c2fa15db56176a0600ee17515c
Taylor Jackson <tjack94@hotmail.com>
2018-10-06 22:37:56 -0600

    added increased redux functionality to step one of the wizard

--- src/Redux/Reducers/reducer.js
+++ src/Redux/Reducers/reducer.js
@@ -6,6 +6,7 @@ function wizardSteps(state = initialState, action) {
 	switch (action.type) {
 		case 'WIZARD_STEP_ONE':
 			return Object.assign({}, state, {
+				address: action.payload.address,
 				longitude: action.payload.longitude,
 				latitude: action.payload.latitude,
 				radius: action.payload.radius,
