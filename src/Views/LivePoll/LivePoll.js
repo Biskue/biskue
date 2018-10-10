3392469ed61fd821a2275419139e9e93e39e3217
Taylor Jackson <tjack94@hotmail.com>
2018-10-09 20:07:27 -0600

    created username modal for poll page and started on restaurant card

--- src/Views/LivePoll/LivePoll.js
+++ src/Views/LivePoll/LivePoll.js
@@ -1,53 +1,82 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 import socketIOClient from 'socket.io-client';
 const socket = socketIOClient('http://localhost:4005/', {
-  extraHeaders: { 'Access-Control-Allow-Credentials': 'omit' }
+	extraHeaders: { 'Access-Control-Allow-Credentials': 'omit' }
 });
 
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)'
+	}
+};
 
 export default class LivePoll extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      response: '',
-      number: 0,
+	constructor(props) {
+		super(props);
+		this.state = {
+			response: '',
+			number: 0,
       pollCode: this.props.match.params.pollCode,
-    }
+      modalIsOpen: false,
+      username: ''
+		};
+	}
+componentWillMount(){
 
+}
+	componentDidMount() {
+		var room = this.state.pollCode;
+		socket.emit('room', room);
+		socket.on('news', (data) => {
+			this.setState({
+				response: data
+			});
+			console.log(data);
+			socket.send('hi');
+
+			socket.on('incremented', (number) => {
+				this.setState({
+					number
+				});
+			});
+		});
+	}
+  handleChange(event, name) {
+		const value = event.target.value;
+		this.setState({ [name]: value });
+	}
+	increment() {
+		socket.emit('increment', this.state.number, this.state.pollCode);
+	}
+
+	render() {
+    const modalButton = this.state.username != '' ? <button onClick={()=> this.saveUsername()}>Go</button> : null
+		return (
+			<div>
+				Hello
+				{this.state.response.hello}
+				<button onClick={() => this.increment()}>Increment Socket</button>
+				{this.state.number}
+				<Modal
+					isOpen={this.state.modalIsOpen}
+					onRequestClose={()=>this.saveUsername()}
+					contentLabel="Import Modal"
+					style={customStyles}
+				>
+					<h3>Type A Username your friends can identify you by</h3>
+          <input type="text" value={this.state.username} onChange= {(e)=> this.handleChange(e, 'username')}/>
+					{modalButton}
+				</Modal>
+			</div>
+		);
   }
-
-  componentDidMount() {
-    var room = this.state.pollCode;
-    socket.emit('room', room);
-    socket.on('news', data  => {
-      this.setState({
-        response: data,
-      })
-      console.log(data);
-      socket.send('hi');
-
-      socket.on('incremented', (number) => {
-      this.setState({
-        number
-      })
-    });
-
-  });
-  }
-
-  increment() {
-    socket.emit('increment', this.state.number, this.state.pollCode)
-  }
-
-  render() {
-    return (
-      <div>
-        Hello 
-        {this.state.response.hello}
-        <button
-          onClick={() =>this.increment() }>Increment Socket</button>
-        {this.state.number}
-      </div>
-    )
+  saveUsername(){
+    this.setState({modalIsOpen: false})
   }
 }
--- src/Views/LivePoll/RestaurantCard.js
+++ src/Views/LivePoll/RestaurantCard.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import upVote from '../../upVote.png'
+import downVote from '../../downVote.png'
 
 export default class RestaurantCard extends Component {
   render() {
+    const categories = props.currentRes.categories.map(cat=> cat.title)
+    const categoriesJoined = categories.join(', ')
     return (
       <div>
-        
+      <img className ='restaurant-image'src={props.currentRes.image_url} alt="yelp image"/>
+      <div>
+          <h4>{props.currentRes.name}</h4>
+          <p>{categoriesJoined}</p>
+          <p>Price: {props.currentRes.price}</p>
+          <p>
+            {props.currentRes.display_phone} <br/>
+            {props.currentRes.location.address1 + ' '+ props.currentRes.location.address2 + ' ' +props.currentRes.location.address3} <br/>
+            {props.currentRes.location.city + ', ' + props.currentRes.location.state + ' ' + props.currentRes.location.zip_code}
+          </p>
+          <img src={upVote} alt="up vote" style='height: 50px; width: 50px'/>
+          <img src={downVote} alt="down vote" style='height: 50px; width: 50px'/>
       </div>
+    </div>
     )
   }
 }
