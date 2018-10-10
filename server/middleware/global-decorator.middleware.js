fa8567672f95c3385c86065ce996090f19c62fc0
breakingBrad <bradvanorman@gmail.com>
2018-10-09 19:46:41 -0600

    updated session with store, helmet, and cookie updates

--- server/middleware/global-decorator.middleware.js
+++ server/middleware/global-decorator.middleware.js
@@ -3,27 +3,35 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const helmet = require('helmet');
+const PostgresStore = require('connect-pg-simple')(session);
 
 const addDb = require('./add-db.middleware');
 
 const corsOptions = {
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000/', // can we disable / remove this?
     optionsSuccessStatus: 200
   }
 
 function globalDecorator(app) {
     app.use(cors(corsOptions));
+    app.use(helmet());
     app.use(bodyParser.json());
     app.use(addDb);
     app.use(express.static(__dirname + '/../build'));
     app.use(cookieParser());
     app.use(session({
+        store: new PostgresStore({
+            conString: process.env.DB_CONNECTION_STRING,
+        }),
         key: 'user_sid',
         secret: process.env.SECRET,
         resave: false,
         saveUninitialized: false,
         cookie: {
-            expires: { maxAge: 30 * 24 * 60 * 60 * 1000 }
+            maxAge: 30 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: true,
         }
     }))
 }
