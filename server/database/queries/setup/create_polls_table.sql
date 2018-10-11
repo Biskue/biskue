776b7df2ca1ac7ab3b5354ade54fabfbace9ed07
breakingBrad <bradvanorman@gmail.com>
2018-10-10 20:06:32 -0600

    updated polls setup query to reflect current state of table

--- server/database/queries/setup/create_polls_table.sql
+++ server/database/queries/setup/create_polls_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE "polls" (
 	"votesPerUser" INTEGER,
 	"allowDownVotes" BOOLEAN,
 	"isActive" BOOLEAN,
-	"participants" JSONB,
+	"participants" text[],
 	"pollWinner" JSONB,
 	"createdAt" TIMESTAMP NOT NULL DEFAULT now(),
 	"updatedAt" TIMESTAMP NOT NULL DEFAULT now()
