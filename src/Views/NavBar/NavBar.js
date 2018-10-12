428560a62de41b515010ba7c24578aae923e7dc2
--global <--global>
2018-10-11 20:46:40 -0600

    created the button to switch from login to logout when logged in, now I just need to get it to trigger that once they actually login on the login page

--- src/Views/NavBar/NavBar.js
+++ src/Views/NavBar/NavBar.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import './NavBar.css';
 import logo from '../../logo.svg'
 import { Link } from 'react-router-dom';
+import LoginControl from './LoginControl';
 
 export class NavBar extends Component {
+
+
   render() {
     return (
       <div className="navbar">
         <Link to='/'>
         <img className="navbar-logo" src={logo} alt="logo" />
         </Link>
-        <Link to='/Login'>
-        <span>Login</span>
-        </Link>
+        <LoginControl/>
+        {/* <Link to='/Login'>
+        
+        </Link> */}
         
       </div>
     )
