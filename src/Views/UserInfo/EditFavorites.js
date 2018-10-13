3a262d81520d07855a17f5e8a46e63f3b4aa0214
Taylor Jackson <tjack94@hotmail.com>
2018-10-13 13:49:13 -0600

    working on ability to select favorites lists

--- src/Views/UserInfo/EditFavorites.js
+++ src/Views/UserInfo/EditFavorites.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import * as Actions from '../../Redux/Actions/actions';
+import { Link } from 'react-router-dom';
 
-export default class EditFavorites extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+class EditFavorites extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			favoritesLists: []
+		};
+	}
+	componentWillMount() {
+		axios.get('lists/savedLists/').then((response) => {
+			this.setState({ favoritesLists: response.data });
+		});
+	}
+
+	render() {
+		const favoritesLists = this.state.favoritesLists.map((list, index) => {
+			return (
+				<div key={index}>
+					{list.listName}{' '}
+					<Link to="/wizard/step-4">
+						<button onClick={() => this.props.saveStepThree(list.listItems)}>Select</button>{' '}
+					</Link>
+				</div>
+			);
+		});
+		return <div>
+      {favoritesLists}
+    </div>;
+	}
 }
+export default connect((state) => state, Actions)(EditFavorites);
--- src/Views/UserInfo/UserInfo.js
+++ src/Views/UserInfo/UserInfo.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import EditFavorites from './EditFavorites'
 
 export default class UserInfo extends Component {
   render() {
     return (
       <div>
-        
+        <EditFavorites/>
       </div>
     )
   }
